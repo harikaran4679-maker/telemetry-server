12890c15e1e464c455994355510a6b4d19dfd3cb
Harikaran S <harikaran4679@gmail.com>
2026-08-07 13:43:04 +0530

    Added InfluxDB logging

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const { InfluxDB, Point } = require("@influxdata/influxdb-client");
 const mqtt = require("mqtt");
 const express = require("express");
 const http = require("http");
@@ -16,6 +17,22 @@ const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+});
+
+// ---------------- InfluxDB ----------------
+
+const influxDB = new InfluxDB({
+  url: process.env.INFLUX_URL,
+  token: process.env.INFLUX_TOKEN
+});
+
+const writeApi = influxDB.getWriteApi(
+  process.env.INFLUX_ORG,
+  process.env.INFLUX_BUCKET
+);
+
+writeApi.useDefaultTags({
+  source: "ESP32"
 });
 
 // ---------------- MQTT ----------------
@@ -35,7 +52,9 @@ client.on("connect", () => {
 });
 
 client.on("message", (topic, message) => {
+
     try {
+
         const data = JSON.parse(message.toString());
 
         console.clear();
@@ -43,7 +62,28 @@ client.on("message", (topic, message) => {
 
         io.emit("telemetry", data);
 
+        const point = new Point("imu");
+
+        if (data.ax !== undefined) point.floatField("ax", data.ax);
+        if (data.ay !== undefined) point.floatField("ay", data.ay);
+        if (data.az !== undefined) point.floatField("az", data.az);
+
+        if (data.gx !== undefined) point.floatField("gx", data.gx);
+        if (data.gy !== undefined) point.floatField("gy", data.gy);
+        if (data.gz !== undefined) point.floatField("gz", data.gz);
+
+        if (data.temperature !== undefined)
+            point.floatField("temperature", data.temperature);
+
+        writeApi.writePoint(point);
+
     } catch (err) {
         console.log(err);
     }
+
+});
+
+process.on("SIGINT", async () => {
+    await writeApi.close();
+    process.exit();
 });
